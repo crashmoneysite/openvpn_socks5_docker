6f48dc57efa0b8fbb15b92586b6a68036992ac87
crashmoneysite <crashmoneysite@gmail.com>
2025-05-04 03:30:54 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,6 +13,10 @@ if [ ! "$(docker ps -q -f name=$IMAGE)" ]; then
     --interactive \
     --detach \
     -p 9050:1080 \
+    -p 8086:40000 \
+    -p 2043:2053 \
+    -p 1070:1090 \
+    -p 1071:1091 \
     --volume "$(pwd)/config:/etc/openvpn" \
     --volume "$(pwd)/config/danted.conf:/etc/danted.conf" \
     $IMAGE
